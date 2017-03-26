b72734568e80508c86de1b5f2b9881e1ed3a23d0
Patrick Force <patrickforce@gmail.com>
2017-03-26 17:17:12 -0600

    expand tests now that we have ability to mock aws-sdk

--- tests/aws/gardener.test.js
+++ tests/aws/gardener.test.js
@@ -6,6 +6,12 @@ var expect  = chai.expect; // we are using the "expect" style of Chai
 var mockery = require('mockery');
 var awsMock = {};
 
+function getAwsGardner(awsMock) {
+    mockery.registerMock('aws-sdk', awsMock);
+    var Gardens = require(path.join('..', '..', 'lib'));
+    return new Gardens.Aws.Gardener('__tests-profile', 'us-east-1');
+}
+
 describe('Aws.Gardener', function() {
 
     beforeEach(function() {
@@ -33,11 +39,149 @@ describe('Aws.Gardener', function() {
                 callback({message: 'already exists'}, null);
             };
         };
-        mockery.registerMock('aws-sdk', awsMock);
-        var Gardens = require(path.join('..', '..', 'lib'));
-        var gardener = new Gardens.Aws.Gardener('__tests-profile', 'us-east-1');
+        var gardener = getAwsGardner(awsMock);
         gardener.createKey('__tests').then(function(result) {
             expect(result).to.include('already exists');
+        }).catch(function(error) {
+            expect(error).to.equal(null);
+        });
+    });
+
+    it('createKey should succeeed in creating key', function() {
+        awsMock.EC2 = function() {
+            this.createKeyPair = function(obj, callback) {
+                callback(null, { KeyMaterial: '__test-key-material' });
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createKey('__tests').then(function(result) {
+            expect(result).to.equal('__test-key-material');
+        }).catch(function(error) {
+            expect(error).to.equal(null);
+        });
+    });
+
+    it('createKey should handle error appropriately', function() {
+        awsMock.EC2 = function() {
+            this.createKeyPair = function(obj, callback) {
+                callback({ message: 'some random error' }, null);
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createKey('__tests').then(function(result) {
+            expect(result).to.equal(null);
+        }).catch(function(error) {
+            expect(error.message).to.equal('some random error');
+        });
+    });
+
+    it('getAccountId should succeeed in getting account id', function() {
+        awsMock.STS = function() {
+            this.getCallerIdentity = function(obj, callback) {
+                callback(null, { Account: '__test-account-data' });
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.getAccountId().then(function(result) {
+            expect(result).to.equal('__test-account-data');
+        }).catch(function(error) {
+            expect(error).to.equal(null);
+        });
+    });
+
+    it('getAccountId should error appropriately', function() {
+        awsMock.STS = function() {
+            this.getCallerIdentity = function(obj, callback) {
+                callback({ message: 'random error message' }, null);
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.getAccountId().then(function(result) {
+            expect(result).to.equal(null);
+        }).catch(function(error) {
+            expect(error.message).to.equal('random error message');
+        });
+    });
+
+    it('createHostedZone should succeed in creating a hosted zone', function() {
+        awsMock.Route53 = function() {
+            this.listHostedZonesByName = function(obj, callback) {
+                callback(null, { HostedZones: []});
+            };
+            this.createHostedZone = function(obj, callback) {
+                callback(null, 'created hosted zone');
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createHostedZone('__tests-hosted-zone').then(function(result) {
+            expect(result).to.equal('created hosted zone');
+        }).catch(function(error) {
+            expect(error).to.equal(null);
+        });
+    });
+
+    it('createHostedZone should return hosted zone data for zone that already exists', function() {
+        awsMock.Route53 = function() {
+            this.listHostedZonesByName = function(obj, callback) {
+                callback(null, { HostedZones: [{ Id: '000000000' }]});
+            };
+            this.getHostedZone = function(obj, callback) {
+                callback(null, '__tests-existing-hosted-zone')
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createHostedZone('__tests-hosted-zone').then(function(result) {
+            expect(result).to.equal('__tests-existing-hosted-zone');
+        }).catch(function(error) {
+            expect(error).to.equal(null);
+        });
+    });
+
+    it('createHostedZone should error appropriately from getting existing list', function() {
+        awsMock.Route53 = function() {
+            this.listHostedZonesByName = function(obj, callback) {
+                callback({ message: 'random error message' }, null);
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createHostedZone('__tests-hosted-zone').then(function(result) {
+            expect(result).to.equal(null);
+        }).catch(function(error) {
+            expect(error.message).to.equal('random error message');
+        });
+    });
+
+    it('createHostedZone should error appropriately from creating new hosted zone', function() {
+        awsMock.Route53 = function() {
+            this.listHostedZonesByName = function(obj, callback) {
+                callback(null, { HostedZones: []});
+            };
+            this.createHostedZone = function(obj, callback) {
+                callback({ message: 'random error message' }, null);
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createHostedZone('__tests-hosted-zone').then(function(result) {
+            expect(result).to.equal(null);
+        }).catch(function(error) {
+            expect(error.message).to.equal('random error message');
+        });
+    });
+
+    it('createHostedZone should error appropriately from getting existing hosted zone', function() {
+        awsMock.Route53 = function() {
+            this.listHostedZonesByName = function(obj, callback) {
+                callback(null, { HostedZones: [{ Id: '000000000' }]});
+            };
+            this.getHostedZone = function(obj, callback) {
+                callback({ message: 'random error message'}, null)
+            };
+        };
+        var gardener = getAwsGardner(awsMock);
+        gardener.createHostedZone('__tests-hosted-zone').then(function(result) {
+            expect(result).to.equal(null);
+        }).catch(function(error) {
+            expect(error.message).to.equal('random error message');
         });
     });
 

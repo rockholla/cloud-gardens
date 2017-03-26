56adaabca5ad91d7b4edb73645872f6d62c952f0
Patrick Force <patrickforce@gmail.com>
2017-03-26 16:54:46 -0600

    begin test mocking of aws-sdk

--- tests/aws/gardener.test.js
+++ tests/aws/gardener.test.js
@@ -3,9 +3,43 @@
 var path    = require('path');
 var chai    = require('chai');
 var expect  = chai.expect; // we are using the "expect" style of Chai
-var Gardens = require(path.join('..', '..', 'lib'));
+var mockery = require('mockery');
+var awsMock = {};
 
 describe('Aws.Gardener', function() {
+
+    beforeEach(function() {
+        mockery.enable({
+            warnOnReplace: false,
+            warnOnUnregistered: false,
+            useCleanCache: true
+        });
+        awsMock = {
+            SharedIniFileCredentials: function() {},
+            config: {
+                credentials: null,
+                update: function() {}
+            },
+            EC2: function() {},
+            S3: function() {},
+            STS: function() {},
+            Route53: function() {}
+        };
+    });
+
+    it('createKey should tell us that the key already exists if it does w/o an error', function() {
+        awsMock.EC2 = function() {
+            this.createKeyPair = function(obj, callback) {
+                callback({message: 'already exists'}, null);
+            };
+        };
+        mockery.registerMock('aws-sdk', awsMock);
+        var Gardens = require(path.join('..', '..', 'lib'));
+        var gardener = new Gardens.Aws.Gardener('__tests-profile', 'us-east-1');
+        gardener.createKey('__tests').then(function(result) {
+            expect(result).to.include('already exists');
+        });
+    });
 
     // TODO: mock aws sdk and terraform to appropriately test this
     /*it('terraform() plan should return true', function() {

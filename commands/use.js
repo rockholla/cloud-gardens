eabcae0de8108b08ec205eae765f5679747835f7
Patrick Force <patrickforce@gmail.com>
2017-05-10 15:24:41 -0600

    make some fixes and improvements to the use command

--- commands/use.js
+++ commands/use.js
@@ -12,15 +12,27 @@ exports.command = 'use [name]';
 exports.desc = 'use a named configuration for gardening tasks';
 exports.handler = function(argv) {
   if (!argv.name) return winston.error('Please enter a name for the new configuration');
-  glob(path.join(__dirname, '..', 'config', 'local.*'), function(error, files) {
-    files.forEach(function(file) {
-      try {
-        fs.unlinkSync(file);
-      } catch (error) {}
-      glob(path.join(__dirname, '..', 'config', argv.name + '.*'), function(error, files) {
-        fs.symlinkSync(files[0], path.join(__dirname, '..', 'config', 'local' + path.extname(files[0])));
-        winston.info(`Now using config "${argv.name}"`);
-      });
-    });
-  });
+
+  try {
+    fs.unlinkSync(path.join(__dirname, '..', 'config', 'local.js'))
+  } catch (error) {}
+  try {
+    fs.unlinkSync(path.join(__dirname, '..', 'config', 'local.json'))
+  } catch (error) {}
+  if (fs.existsSync(path.join(__dirname, '..', 'config', argv.name + '.js'))) {
+    fs.symlinkSync(
+      path.join(__dirname, '..', 'config', argv.name + '.js'),
+      path.join(__dirname, '..', 'config', 'local.js')
+    );
+    winston.info(`Now using config "${argv.name}"`);
+  } else if (fs.existsSync(path.join(__dirname, '..', 'config', argv.name + '.json'))) {
+    fs.symlinkSync(
+      path.join(__dirname, '..', 'config', argv.name + '.json'),
+      path.join(__dirname, '..', 'config', 'local.json')
+    );
+    winston.info(`Now using config "${argv.name}"`);
+  } else {
+    return winston.error(`Unable to find config file with the name "${argv.name}"`);
+  }
+
 };

a30e2595af3cfc714245d5db028d94c73e7ba644
Patrick Force <patrickforce@gmail.com>
2017-09-18 20:39:46 -0600

    random fixes

--- gardener.js
+++ gardener.js
@@ -10,28 +10,30 @@ var mkdirp  = require('mkdirp');
 winston.setLevels({
   error: 0, warn: 1, info: 2, verbose: 3, debug: 4
 });
-winston.addColors({
-  error: 'red', warn: 'yellow', info: 'green', verbose: 'cyan', debug: 'blue'
-});
-winston.remove(winston.transports.Console)
-winston.add(winston.transports.Console, {
-  level: config.log.level,
-  prettyPrint: true,
-  colorize: true,
-  silent: false,
-  timestamp: false
-});
-winston.add(winston.transports.File, {
-  prettyPrint: false,
-  level: config.log.level,
-  silent: false,
-  colorize: false,
-  timestamp: true,
-  filename: './logs/' + pkgjson.name + '.log',
-  maxsize: 40000,
-  maxFiles: 10,
-  json: false
-});
+winston.addColors(config.log.colors);
+winston.remove(winston.transports.Console);
+if (config.log.console) {
+  winston.add(winston.transports.Console, {
+    level: config.log.level,
+    prettyPrint: true,
+    colorize: true,
+    silent: false,
+    timestamp: false
+  });
+}
+if (config.log.file) {
+  winston.add(winston.transports.File, {
+    prettyPrint: false,
+    level: config.log.level,
+    silent: false,
+    colorize: false,
+    timestamp: true,
+    filename: './logs/' + pkgjson.name + '.log',
+    maxsize: 40000,
+    maxFiles: 10,
+    json: false
+  });
+}
 
 var argv = require('yargs')
   .usage('Usage: $0 <command> [options]')

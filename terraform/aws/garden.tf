c742d3690c373f537d6378dc088775296c2f0e07
Patrick Force <patrickforce@gmail.com>
2017-11-06 14:32:03 -0700

    update ecs optimized ami list

--- terraform/aws/garden.tf
+++ terraform/aws/garden.tf
@@ -153,17 +153,18 @@ variable "ecs_ami" {
 
 variable "default_ecs_ami" {
   default = {
-    us-east-1       = "ami-9eb4b1e5"
-    us-east-2       = "ami-1c002379"
-    us-west-1       = "ami-4a2c192a"
-    us-west-2       = "ami-1d668865"
-    eu-west-1       = "ami-8fcc32f6"
-    eu-west-2       = "ami-cb1101af"
-    eu-central-1    = "ami-0460cb6b"
-    ap-northeast-1  = "ami-b743bed1"
-    ap-southeast-1  = "ami-9d1f7efe"
-    ap-southeast-2  = "ami-c1a6bda2"
-    ca-central-1    = "ami-b677c9d2"
+    us-east-2       = "ami-b0527dd5"
+    us-east-1       = "ami-20ff515a"
+    us-west-2       = "ami-3702ca4f"
+    us-west-1       = "ami-b388b4d3"
+    eu-west-2       = "ami-ee7d618a"
+    eu-west-1       = "ami-d65dfbaf"
+    eu-central-1    = "ami-ebfb7e84"
+    ap-northeast-2  = "ami-70d0741e"
+    ap-northeast-1  = "ami-95903df3"
+    ap-southeast-2  = "ami-e3b75981"
+    ap-southeast-1  = "ami-c8c98bab"
+    ca-central-1    = "ami-fc5fe798"
   }
 }
 
